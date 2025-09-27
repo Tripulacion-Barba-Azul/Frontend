ec75cd547e71c7dc5f5fdda28382358e7bf5ccca
MarcoSca35 <marcoscavuzzo@mi.unc.edu.ar>
2025-09-27 01:32:41 -0300

    Added gitignore and reorganized the images directories

--- src/components/ViewSecrets/ViewSecrets.jsx
+++ src/components/ViewSecrets/ViewSecrets.jsx
@@ -1,18 +1,13 @@
 import { useState } from "react";
-import './ViewSecrets.css'
-
-import secret_front from '../../assets/cards/05-secret_front.png'
-import secret_back from '../../assets/cards/06-secret_back.png'
-import secret_accompice from '../../assets/cards/04-secret_accomplice.png'
-import secret_murderer from '../../assets/cards/03-secret_murderer.png'
-import shh_icon from '../../assets/icons/shh.png'
+import "./ViewSecrets.css";
 
 const imageMap = {
-  "murderer" : secret_murderer,
-  "accomplice" : secret_accompice,
-  "regular" : secret_back
-}
-
+  murderer: "../../../public/Cards/03-secret_murderer.png",
+  accomplice: "../../../public/Cards/04-secret_accomplice.png",
+  regular: "../../../public/Cards/06-secret_back.png",
+  secretFront: "../../../public/Cards/05-secret_front.png",
+  shhIcon: "../../../public//Icons/shhIcon.png",
+};
 
 export default function ViewSecrets({ secrets }) {
   const [ViewSecrets, setViewSecrets] = useState(false);
@@ -21,50 +16,54 @@ export default function ViewSecrets({ secrets }) {
     setViewSecrets(!ViewSecrets);
   };
 
-  const revealedCount = secrets.filter(secret => secret.revealed).length;
+  const revealedCount = secrets.filter((secret) => secret.revealed).length;
   const hiddenCount = secrets.length - revealedCount;
 
   if (ViewSecrets) {
-    document.body.classList.add('active-viewsecrets')
+    document.body.classList.add("active-viewsecrets");
   } else {
-    document.body.classList.remove('active-viewsecrets')
+    document.body.classList.remove("active-viewsecrets");
   }
 
   return (
     <>
       <button onClick={toggleViewSecrets} className="btn-viewsecrets">
         <div className="light-dots">
-          {secrets.map(secret => (
-            <div 
+          {secrets.map((secret) => (
+            <div
               key={secret.class}
-              className={`light-dot ${secret.revealed ? 'revealed' : 'hidden'}`}
-              title={secret.revealed ? `Secret ${secret.class}` : `Secret hidden`}
+              className={`light-dot ${secret.revealed ? "revealed" : "hidden"}`}
+              title={
+                secret.revealed ? `Secret ${secret.class}` : `Secret hidden`
+              }
             />
           ))}
         </div>
-        
-        <img src={shh_icon} alt="shh" />
 
+        <img src={imageMap["shhIcon"]} alt="secretIcon" />
       </button>
 
       {ViewSecrets && (
         <div className="viewsecrets">
           <div onClick={toggleViewSecrets} className="overlay"></div>
           <div className="secrets-grid">
-            {secrets.map(secret => (
+            {secrets.map((secret) => (
               <div key={secret.class} className="secret-card">
                 {secret.revealed ? (
-                  <img src={imageMap[secret.class]} alt={`Secret ${secret.class}`} />
+                  <img
+                    src={imageMap[secret.class]}
+                    alt={`Secret ${secret.class}`}
+                  />
                 ) : (
-                  <img src={secret_front} alt={`Secret hidden`} />
+                  <img src={imageMap["secretFront"]} alt={`Secret hidden`} />
                 )}
               </div>
             ))}
           </div>
-            <button className="close-viewsecrets" onClick={toggleViewSecrets}>
-              X
-            </button>
-          </div>
+          <button className="close-viewsecrets" onClick={toggleViewSecrets}>
+            X
+          </button>
+        </div>
       )}
     </>
   );
--- src/containters/App/App.jsx
+++ src/containters/App/App.jsx
@@ -1,24 +1,5 @@
-import ViewSecrets from '../../components/ViewSecrets/ViewSecrets';
-
 function App() {
-
-  const secretsList = [
-    { id: 1, revealed: true },
-    { id: 2, revealed: false },
-    { id: 3, revealed: true },
-    { id: 1, revealed: true },
-    { id: 2, revealed: false },
-    { id: 3, revealed: true },
-    { id: 1, revealed: true },
-    { id: 2, revealed: false },
-    { id: 3, revealed: true },
-  ]  
-
-  return (
-    <div>
-      <ViewSecrets secrets={secretsList} />
-    </div>
-  )
+  return <div></div>;
 }
 
-export default App
+export default App;
