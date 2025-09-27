05c5bc3bef26a1c1f76f5316b11ec050b8492bf6
Juakinsote <joavazquez@mi.unc.edu.ar>
2025-09-27 20:52:42 -0300

    ING-22 mazo regular + pila descarte (3 etapas, imagenes custom) todo listo

--- src/containters/App/App.jsx
+++ src/containters/App/App.jsx
@@ -1,10 +1,26 @@
-import TitleScreen from '../../components/TitleScreen/TitleScreen';
+import DiscardPile from '../../components/DiscardPile/DiscardPile';
 
 function App() {
 
   return (
     <div>
-      <TitleScreen />
+            <br />
+            <br />
+            <br />
+      <DiscardPile number="3" img_id="7" />
+            <br />
+            <br />
+            <br />
+      <DiscardPile number="15" img_id="8"/>
+            <br />
+            <br />
+            <br />
+      <DiscardPile number="50" img_id="24"/>
+            <br />
+            <br />
+            <br />
+      <DiscardPile number="0" img_id="7"/>
+
     </div>
   )
 }
