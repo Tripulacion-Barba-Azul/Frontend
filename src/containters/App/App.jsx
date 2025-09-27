ee69cd7420246daac7e5cd8a61e5964ba449fccd
MarcoSca35 <marcoscavuzzo@mi.unc.edu.ar>
2025-09-27 09:21:47 -0300

    Added componente that allows to see cards for owner's view and optionally a zoom-in window for each card. Implemented the whole functionality with tests included

--- src/containters/App/App.jsx
+++ src/containters/App/App.jsx
@@ -1,12 +1,23 @@
-import TitleScreen from '../../components/TitleScreen/TitleScreen';
+import OwnCards from "../../components/OwnCards/OwnCards.jsx"; // <- importa el componente
 
 function App() {
+  const cardIds = [11, 16, 23, 21, 19, 8];
 
   return (
-    <div>
-      <TitleScreen />
+    <div className="relative w-full h-screen overflow-hidden">
+      <div
+        style={{
+          backgroundImage: `url("/Board/backgroundBoard.png")`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "bottom",
+          backgroundSize: "cover",
+        }}
+        className="w-full h-screen bg-black"
+      />
+
+      <OwnCards cardIds={cardIds} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
