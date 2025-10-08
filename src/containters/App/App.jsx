7ff773d78ec39c662c356786614a3ec56307a2d4
MarcoSca35 <marcoscavuzzo@mi.unc.edu.ar>
2025-10-08 19:31:53 -0300

    Small fix cause of recent merge.

--- src/containters/App/App.jsx
+++ src/containters/App/App.jsx
@@ -4,20 +4,18 @@ import CreateGameScreen from "../../components/PreGameScreen/CreateGameScreen/Cr
 import JoinGameScreen from "../../components/PreGameScreen/JoinGameScreen/JoinGameScreen";
 import GameMatchesList from "../../components/GameMatchesList/GameMatchesList";
 import GameScreen from "../../components/GameScreen/GameScreen";
-import ExamplePageOrchestrator from "../../components/Sync/ExamplePageOrchestrator";
 
 function App() {
   return (
-    <ExamplePageOrchestrator />
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<TitleScreen />} />
-    //     <Route path="/create" element={<CreateGameScreen />} />
-    //     <Route path="/join" element={<GameMatchesList />} />
-    //     <Route path="/join/:gameId" element={<JoinGameScreen />} />
-    //     <Route path="/game/:gameId" element={<GameScreen />} />
-    //   </Routes>
-    // </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TitleScreen />} />
+        <Route path="/create" element={<CreateGameScreen />} />
+        <Route path="/join" element={<GameMatchesList />} />
+        <Route path="/join/:gameId" element={<JoinGameScreen />} />
+        <Route path="/game/:gameId" element={<GameScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
