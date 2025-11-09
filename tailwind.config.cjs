729090ca5f819e18fdd43622299bcca45f824d3e
Juakinsote <joavazquez@mi.unc.edu.ar>
2025-11-09 17:12:56 -0300

    ING-182 implementada funcionalidad del cursor con placeholders

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,20 @@
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
-  plugins: [],
-};
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    cursor: {
+      auto: 'url("/Cursors/normal.png"), auto',
+      default: 'url("/Cursors/normal.png"), auto',
+      pointer: 'url("/Cursors/pointer.png"), pointer',
+      wait: 'url("/Cursors/normal.png"), wait',
+      text: 'url("/Cursors/normal.png"), text',
+      move: 'url("/Cursors/normal.png"), move',
+      'not-allowed': 'url("/Cursors/normal.png"), not-allowed',
+      grab: 'url("/Cursors/pointer.png"), grab',
+      grabbing: 'url("/Cursors/pointer.png"), grabbing',
+    },
+  },
+}
