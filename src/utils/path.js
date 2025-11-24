469ffb3dcf87e9b6bddcb81b2d878916c3480d44
MarcoSca35 <marcoscavuzzo3009@mi.unc.edu.ar>
2025-11-24 01:41:42 -0300

    Small fixes

--- src/utils/path.js
+++ src/utils/path.js
@@ -1,2 +1,4 @@
-export const URL = "http://localhost:8000";
-export const wsURL =  "ws://localhost:8000/ws";
+// path.js
+export const URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+export const wsURL =
+  import.meta.env.VITE_WS_BASE_URL || "ws://localhost:8000/ws";
